79de8a8eae3fd40ee7f768002eacac61ee9a24ea
Jisu Yeo <duwltn1301@naver.com>
2023-08-22 15:10:56 +0900

    refactor: card 위젯을 다른 파일에서 위젯화 시키기

---
+++ Wallet UI/widgets/currency_card.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+  final double offset;
+  final blackColor = const Color(0xFF1F2123);
+
+  const CurrencyCard(
+      {super.key,
+      required this.name,
+      required this.code,
+      required this.amount,
+      required this.icon,
+      required this.isInverted,
+      required this.offset});
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, offset),
+      child: Container(
+        // 컨테이너 밖으로 튀어나온 아이콘 잘라줌
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: isInverted ? blackColor : Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? blackColor : Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? blackColor
+                              : Colors.white.withOpacity(0.8),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              // 아이콘 크기만 키우기
+              Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  // 아이콘 위치 변경하기
+                  offset: const Offset(-5, 12),
+                  child: Icon(
+                    icon,
+                    color: isInverted ? blackColor : Colors.white,
+                    size: 88,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
