43c47d3d59bf64c671eadaa6365e19d72d375a23
Jisu Yeo <duwltn1301@naver.com>
2023-08-23 20:18:56 +0900

    feat: App Bar 구성하기

---
+++ Webtoon App/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text("오늘의 웹툰",
+            style: TextStyle(
+              fontSize: 24,
+            )),
+      ),
+    );
+  }
+}
