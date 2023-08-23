a9d85cb380dba7d79beaaea01db39c2b22372ead
Jisu Yeo <duwltn1301@naver.com>
2023-08-23 20:18:33 +0900

    feat: home screen 추가하기

---
+++ Webtoon App/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+// flutter SDK 에 있는 3개의 core Widget 중 하나를 상송 받아야한다.
+// 여기는 root 위젯이다.
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // 모든 Widget은 build 메소드를 구현해야한다.
+  @override // auto complete 됨.
+  Widget build(BuildContext context) {
+    // material(구글) 또는 cupertino(ios) 위젯을 return
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
