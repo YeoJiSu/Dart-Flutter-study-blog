4a983fe4a0e5683db055a462e1cc333320c1f90c
Jisu Yeo <duwltn1301@naver.com>
2023-08-24 12:01:34 +0900

    feat: url 결과 reponse가 성공적이면 리스트에 각 웹툰별 정보 저장

--- Webtoon App/services/api_service.dart
+++ Webtoon App/services/api_service.dart
@@ -1,16 +1,24 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
   // pub.dev에서 Dart나 Flutter 패키지 찾아볼 수 있다.
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url); // Future 로 리턴되는 것을 await 사용
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      // dynamic은 어떤 타입이든 수용할 수  있다.
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
